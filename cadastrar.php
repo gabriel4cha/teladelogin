1c7968233b7547dbaf4077d995cdd299e34bfe9a
gabriel4cha <75120826+gabriel4cha@users.noreply.github.com>
2020-11-27 07:38:10 -0300

    Create cadastrar.php

---
+++ cadastrar.php
@@ -0,0 +1,70 @@
+<?php
+	require_once 'CLASSES/usuarios.php';
+	$u = new usuarios;
+?>
+
+<html lang="pt-br">
+<head>
+	<meta charset="utf-8"/>
+	<title>Projeto Login</title>
+	<link rel="stylesheet" href="CSS/estilo.css">
+</head>
+<body>
+<div id="corpo-form-cad">
+	<h1>Cadastrar</h1>
+	<form method="POST">
+	<input type="text" name="nome" placeholder ="Nome Completo" maxlength="30">
+	<input type="text" name="telefone" placeholder ="Telefone" maxlength="30">	
+	<input type="email" name="email" placeholder ="Usuário" maxlength="40">		
+	<input type="password" name="senha" placeholder ="Senha" maxlength="15">
+	<input type="password" name="confSenha" placeholder ="Confirmar Senha" maxlength="15">	
+	<input type="submit" value="Cadastrar">
+	
+	</form>
+
+<?php
+// verificar se clicou no botao
+if(isset($_POST['nome']))
+{
+	$nome = addslashes($_POST['nome']);
+	$telefone = addslashes($_POST['telefone']);
+	$email = addslashes($_POST['email']);
+	$senha = addslashes($_POST['senha']);
+	$confimarSenha = addslashes($_POST['confSenha']);
+	// verificar se esta preenchido 
+	if(!empty($nome) && !empty($telefone) && !empty($email) && !empty($senha) && !empty($confirmarSenha))
+	{
+		$u->conectar("projeto_login", "localhost", "root","");
+		if ($u->msgErro == "")
+		 {
+		 	if($senha == $confirmarSenha)
+		 	{
+				if($u->cadastrar($nome, $telefone, $email, $senha))
+				{
+					echo "Cadastrado com Sucesso! Acesse para entrar!";
+				}
+				else
+				{
+					echo "Email ja cadastrado!";
+				}
+		 	}
+		 	else
+		 	{
+		 		echo "Senha e confimar senha não correspondem";
+		 	}
+		}
+		else
+		{
+			echo "Erro: ".$u->msgErro;
+		}
+	}
+	else
+	{
+	echo "Preencha todos os campos!";
+	}
+}
+
+?>
+</div>
+</body>
+</html>
