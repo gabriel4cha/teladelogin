a051518a9b65c9635f8807bfe02e221defa1d10e
gabriel4cha <75120826+gabriel4cha@users.noreply.github.com>
2020-11-27 07:38:57 -0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,19 @@
+
+<html lang="pt-br">
+<head>
+	<meta charset="utf-8"/>
+	<title>Projeto Login</title>
+	<link rel="stylesheet" href="CSS/estilo.css">
+</head>
+<body>
+<div id="corpo-form">
+	<h1>Entrar</h1>
+	<form method="POST" action="processa.php">
+		<input type="email" placeholder ="Usuário" name="email">	
+		<input type="password" placeholder ="Senha" name="senha">
+		<input type="submit" value="ACESSAR">
+		<a href="cadastrar.php">Ainda não é inscrito?<strong> Cadastre-se!</strong>
+	</form>
+</div>
+</body>
+</html>
